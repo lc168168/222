78d228d5f10467df65d2de4076cb92053de7b33e
lc168168 <773327594@qq.com>
2020-05-21 16:24:27 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div>
+		home
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+ 	data () {
+ 		return{
+ 				
+ 		}
+ 	}
+}
+</script>
+
+<style scoped lang='less'>
+
+</style>
